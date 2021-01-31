203408e900759182a65b95d5f27c7476a7e3cc4a
MVP-28 <gola.88@mail.ru>
2021-01-31 21:39:40 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+
+  $('.about-video__inner').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/slide-left.svg" alt="Левая стрелка"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/slide-right.svg" alt="Правая стрелка"></button>'
+  });
+
+  
+
+});
